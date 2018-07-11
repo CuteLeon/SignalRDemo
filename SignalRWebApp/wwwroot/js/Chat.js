dfdd32ba94a427abad963e396ca5c15be011efbd
Leon <xiaoyankeji@hotmail.com>
2018-07-11 21:59:25 +0800

    遇到一个问题：在360浏览器兼容模式下chat.js=>element.addEventListener()绑定事件失败，等待解决；

--- SignalRWebApp/wwwroot/js/Chat.js
+++ SignalRWebApp/wwwroot/js/Chat.js
@@ -3,6 +3,7 @@
     .build();
 
 connection.on("ReceiveMessage", (user, message) => {
+    //alert("收到一条消息耶！！！\n" + user + ":\n" + message)
     const msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
     const encodedMsg = user + " says " + msg;
     const li = document.createElement("li");
@@ -12,9 +13,20 @@ connection.on("ReceiveMessage", (user, message) => {
 
 connection.start().catch(err => console.error(err.toString()));
 
+/*
+var ele = document.getElementById("sendButton");
+ele.onclick = function () { alert(".onclick") };
+ele.click = function () { alert(".click") };
+ele.attachEvent("onclick", function () { alert("attachEvent(onclick)") });
+ele.attachEvent("click", function () { alert("attachEvent(click)") });
+ele["click"] = function () { alert("[click]") };
+ele["onclick"] = function () { alert("[onclick]") };
+ */
+
 document.getElementById("sendButton").addEventListener("click", event => {
     const user = document.getElementById("userInput").value;
     const message = document.getElementById("messageInput").value;
+    //alert("发送消息：\n" + user + ":\n" + message)
     connection.invoke("SendMessage", user, message).catch(err => console.error(err.toString()));
     event.preventDefault();
 });
